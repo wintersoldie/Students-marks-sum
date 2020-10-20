e642fda11e3d3a0f905b28a5eb5f21e7f8aaf715
ShreyashRaghubanshi <62500668+ShreyashRaghubanshi@users.noreply.github.com>
2020-10-20 11:00:56 +0530

    Update stud.c

--- stud.c
+++ stud.c
@@ -7,7 +7,13 @@ int marks_summation(int* marks, int number_of_students, char gender) {
         for(int i=1;i<number_of_students;i=i+2){
             sum+=*(marks+i);
         }
-    
+    }
+    else if(gender==98){
+        for(int i=0;i<number_of_students;i=i+2){
+            sum+=*(marks+i);
+        }
+       
+    }
 
 
   //Write your code here.
